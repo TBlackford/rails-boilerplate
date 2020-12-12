c5ad94cb92d5e5b41a3c5e059381ab5601b80b48
TBlackford <iamtomblackford@gmail.com>
2020-12-13 12:00:59 +1300

    Updated login, signup, and password change forms and styles

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
       resources :users
 
       root to: "users#index"
-    end
-  #devise_for :users
+  end
+
+  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get 'terms', to: 'home#terms'
 end
