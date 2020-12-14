87faa1dca39ee02fe4435fa88af8356e4130d57d
TBlackford <iamtomblackford@gmail.com>
2020-12-14 21:23:24 +1300

    Updated indentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  namespace :admin do
-      resources :users
+    namespace :admin do
+        resources :users
 
-      root to: "users#index"
-  end
+        root to: "users#index"
+    end
 
-  devise_for :users
+    devise_for :users
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  get 'terms', to: 'home#terms'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    root 'home#index'
+    get 'terms', to: 'home#terms'
 end
